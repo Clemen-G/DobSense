220bdf7c23d423771419953b520ee0c8e564e3e3
Clemen-G <3084704+Clemen-G@users.noreply.github.com>
2023-09-06 18:11:57 +0200

    push_to: extracted optimizer into its own class

--- backend/app/alignment/push_to.py
+++ backend/app/alignment/push_to.py
@@ -14,6 +14,14 @@ Y = [0, 1.0, 0]
 Z = [0, 0, 1.0]
 
 
+def norm(np_matrix):
+    return np.linalg.norm(np_matrix, ord='fro')
+
+
+def ppp(arr):
+    print(np.round(arr.reshape(3, 3), 3))
+
+
 def rot(axis, angle):
     """Generates the matrix for a rotation about an axis
 
@@ -189,66 +197,78 @@ gradient_penalties_lambda = wrap_with_numpy(
 # %%
 
 
-def ppp(arr):
-    print(np.round(arr.reshape(3, 3), 3))
+class AlignmentFinder():
+    def __init__(self, gradient_optimized_err_lambda,
+                 gradient_penalties_lambda, hyperparameters):
+        self.gradient_optimized_err_lambda = gradient_optimized_err_lambda
+        self.gradient_penalties_lambda = gradient_penalties_lambda
+        self.hyperparameters = hyperparameters
 
+    # coordinates is 2d array with lines
+    # cos(t_az), cos(t_alt), sin(t_az), sin(t_az), az_x, az_y, az_z
+    # where az_x, _y, _z represent the unit vector of the star in the alt-az
+    # reference frame
+    def get_alignment_matrices(self, coordinates, debug=False):
+        """Determines the change of base matrices associated to the current telescope setup.
 
-# coordinates is 2d array with lines
-# cos(t_az), cos(t_alt), sin(t_az), sin(t_az), az_x, az_y, az_z
-# where az_x, _y, _z represent the unit vector of the star in the alt-az
-# reference frame
-def optimize(hyperparameters, coordinates, debug=False):
-    def gradient_at(gradient_err, gradient_penalties, points, theta_v):
-        gradient_v = reduce(
-            np.ndarray.__add__,
-            [gradient_err(np.hstack((p, theta_v))) for p in points])
+        Args:
+            coordinates: numpy array shape=(7, n), where one row is
+            cos(t_az), cos(t_alt), sin(t_az), sin(t_az), az_x, az_y, az_z
+            t_* represent the telescope angles, az_* are alt-az coords of the object.
+            az_* must form a unit vector.
+            debug: prints additional debug. Defaults to False.
+        """
+        def gradient_at(gradient_err, gradient_penalties, points, theta_v):
+            gradient_v = reduce(
+                np.ndarray.__add__,
+                [gradient_err(np.hstack((p, theta_v))) for p in points])
 
-        if hyperparameters["n_samples_correction"]:
-            gradient_v /= points.shape[0] # divide by number of points to get avg
+            if self.hyperparameters["n_samples_correction"]:
+                gradient_v /= points.shape[0] # divide by number of points to get avg
 
-        penalty_v = reduce(np.ndarray.__add__,
-                           [g(theta_v) for g in gradient_penalties])
-        gradient_v += hyperparameters["penalty_weight"] * penalty_v
-        return gradient_v
+            penalty_v = reduce(np.ndarray.__add__,
+                            [g(theta_v) for g in gradient_penalties])
+            gradient_v += self.hyperparameters["penalty_weight"] * penalty_v
+            return gradient_v
 
-    theta_v = np.zeros(len(theta))
-    theta_v[0] = 1  # setting the initial R_azO to identity or it's degenerate
-    theta_v[4] = 1
-    theta_v[8] = 1
-    theta_v[9] = 1  # setting the initial R_tilt to identity or it's degenerate
-    theta_v[11] = 1  # setting the initial R_altO to identity or it's degenerate
+        theta_v = np.zeros(len(theta))
+        theta_v[0] = 1  # setting the initial R_azO to identity or it's degenerate
+        theta_v[4] = 1
+        theta_v[8] = 1
+        theta_v[9] = 1  # setting the initial R_tilt to identity or it's degenerate
+        theta_v[11] = 1  # setting the initial R_altO to identity or it's degenerate
 
-    v = np.zeros(len(theta))
-    if hyperparameters["use_optimized"]:
-        g_err = gradient_optimized_err_lambda
-        g_pen = [gradient_penalties_lambda]
-    else:
-        g_err = gradient_err_lambda
-        g_pen = [gradient_R_azO_penalties_lambda,
-                 gradient_R_altO_penalties_lambda,
-                 gradient_R_tilt_penalties_lambda,
-                 gradient_R_azO_alignment_penalties_lambda,
-                 ]
-    for _ in range(0, hyperparameters["num_steps"]):
-        gradient = gradient_at(g_err,
-                               g_pen,
-                               coordinates,
-                               theta_v)
-        v = hyperparameters["beta"] * v + \
-            (1-hyperparameters["beta"]) * gradient
-        if debug:
-            print(_)
-            print(np.linalg.norm(gradient, 2))
-        theta_v = theta_v - hyperparameters["alpha"] * v
+        v = np.zeros(len(theta))
+        if self.hyperparameters["use_optimized"]:
+            g_err = self.gradient_optimized_err_lambda
+            g_pen = [self.gradient_penalties_lambda]
+        else:
+            g_err = gradient_err_lambda
+            g_pen = [gradient_R_azO_penalties_lambda,
+                    gradient_R_altO_penalties_lambda,
+                    gradient_R_tilt_penalties_lambda,
+                    gradient_R_azO_alignment_penalties_lambda,
+                    ]
+        for _ in range(0, self.hyperparameters["num_steps"]):
+            gradient = gradient_at(g_err,
+                                g_pen,
+                                coordinates,
+                                theta_v)
+            v = self.hyperparameters["beta"] * v + \
+                (1 - self.hyperparameters["beta"]) * gradient
+            if debug:
+                print(_)
+                print(np.linalg.norm(gradient, 2))
+            theta_v = theta_v - self.hyperparameters["alpha"] * v
 
-    R_azO_s_est = theta_v[0:9].reshape(3, 3).T
-    R_tilt_s_est = np.array(
-        R_tilt.subs([(t1, theta_v[9]), (t2, theta_v[10])]).evalf()
-    ).astype(np.float64)
-    R_altO_s_est = np.array(
-        R_altO.subs([(t3, theta_v[11]), (t4, theta_v[12])]).evalf()
-    ).astype(np.float64)
-    return R_azO_s_est, R_tilt_s_est, R_altO_s_est
+        R_azO_s_est = theta_v[0:9].reshape(3, 3).T
+        R_tilt_s_est = np.array(
+            R_tilt.subs([(t1, theta_v[9]), (t2, theta_v[10])]).evalf()
+        ).astype(np.float64)
+        R_altO_s_est = np.array(
+            R_altO.subs([(t3, theta_v[11]), (t4, theta_v[12])]).evalf()
+        ).astype(np.float64)
+        return R_azO_s_est, R_tilt_s_est, R_altO_s_est
 
 
 def get_star_coords_from_taz(R_azO_s, R_altO_s, R_tilt_s, telescope_angles):
@@ -337,10 +357,6 @@ def generate_alignment_samples(n_samples=1, base_seed=None, index=None):
     return samples
 
 
-def norm(np_matrix):
-    return np.linalg.norm(np_matrix, ord='fro')
-
-
 # %%
 
 hyperparameters = {
@@ -362,9 +378,12 @@ def run():
         test_sample.pop('R_azO_est', None)
         test_sample.pop('R_tilt_est', None)
         test_sample.pop('R_altO_est', None)
-        R_azO_est, R_tilt_est, R_altO_est = optimize(
-            hyperparameters=hyperparameters,
-            coordinates=test_sample["taz_star_coordinates"])
+        alignment_finder = AlignmentFinder(gradient_optimized_err_lambda,
+                                           gradient_penalties_lambda,
+                                           hyperparameters)
+        R_azO_est, R_tilt_est, R_altO_est = \
+            alignment_finder.get_alignment_matrices(
+                coordinates=test_sample["taz_star_coordinates"])
         test_sample['R_azO_est'] = R_azO_est
         test_sample['R_tilt_est'] = R_tilt_est
         test_sample['R_altO_est'] = R_altO_est
@@ -404,10 +423,13 @@ hyperparameters = {
 
 test_sample = generate_alignment_samples()[0]
 print(test_sample)
-R_azO_s_est, R_tilt_s_est, R_altO_s_est = optimize(
-    hyperparameters=hyperparameters,
-    coordinates=test_sample["taz_star_coordinates"])
 
+alignment_finder = AlignmentFinder(gradient_optimized_err_lambda,
+                                    gradient_penalties_lambda,
+                                    hyperparameters)
+R_azO_s_est, R_tilt_s_est, R_altO_s_est = \
+            alignment_finder.get_alignment_matrices(
+                coordinates=test_sample["taz_star_coordinates"])
 
 R_azO_s, R_tilt_s, R_altO_s = test_sample["R_azO"], test_sample["R_tilt"], test_sample["R_altO"],
 print("Estimated azO:")
@@ -496,8 +518,12 @@ angle_cosines[:, [2, 1]] = angle_cosines[:, [1, 2]]
 
 sim_points = np.hstack((angle_cosines, sim_points))
 
-R_azO_est, R_tilt_est, R_altO_est = optimize(hyperparameters=hyperparameters,
-                                             coordinates=sim_points)
+alignment_finder = AlignmentFinder(gradient_optimized_err_lambda,
+                                    gradient_penalties_lambda,
+                                    hyperparameters)
+R_azO_est, R_tilt_est, R_altO_est = \
+            alignment_finder.get_alignment_matrices(
+                coordinates=sim_points)
 
 print(R_azO_est - R_azO_s)
 print(R_azO_est)

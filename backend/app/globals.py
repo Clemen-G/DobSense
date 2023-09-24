f23be05d269af996de62e37433ddf6e3f7063891
Clemen-G <3084704+Clemen-G@users.noreply.github.com>
2023-09-24 08:40:24 -0400

    websocket handler sends state update at alignment change

--- backend/app/globals.py
+++ backend/app/globals.py
@@ -1,13 +1,18 @@
 import os
 import json
+import logging
 from data_model import AlignmentPoints
 
 
 class SystemState:
+    ALIGN_CHANGE = "ALIGN_CHANGE"
+    events = set([ALIGN_CHANGE])
+
     def __init__(self):
         self._alignment_points = AlignmentPoints()
         self._alignment_matrices = None
         self._location = None
+        self._event_listeners = {e: set() for e in self.__class__.events}
 
     @property
     def alignment_matrices(self):
@@ -16,6 +21,7 @@ class SystemState:
     @alignment_matrices.setter
     def alignment_matrices(self, value):
         self._alignment_matrices = value
+        self._notify_listeners(self.__class__.ALIGN_CHANGE)
 
     @property
     def location(self):
@@ -30,6 +36,24 @@ class SystemState:
     @property
     def alignment_points(self):
         return self._alignment_points
+
+    def register(self, event_name, handler):
+        if event_name not in self.__class__.events:
+            raise ValueError("unknown event " + event_name)
+        if handler in self._event_listeners[event_name]:
+            raise ValueError("handler already registered for this event")
+        self._event_listeners[event_name].add(handler)
+    
+    def unregister(self, event_name, handler):
+        if event_name not in self.__class__.events:
+            raise ValueError("unknown event " + event_name)
+        if handler not in self._event_listeners[event_name]:
+            raise ValueError("handler not registered for this event")
+        self._event_listeners[event_name].remove(handler)
+    
+    def _notify_listeners(self, event_name):
+        for listener in self._event_listeners[event_name]:
+            listener(event_name)
 
 
 class Catalogs:
--- backend/app/handlers.py
+++ backend/app/handlers.py
@@ -11,6 +11,7 @@ from data_model import AlignmentPoint, IsAligned, TelescopeCoords, TazCoords,\
     EqCoords
 from data_model import AltAzCoords
 import alignment.coordinates as coordinates
+from globals import SystemState
 
 
 class AppHandler(tornado.web.RequestHandler):
@@ -132,16 +133,18 @@ class WebsocketHandler(websocket.WebSocketHandler):
     
     def open(self):
         logging.info("WebSocket opened")
+        self.globals.state.register(
+            SystemState.ALIGN_CHANGE,
+            self._on_state_change)
 
     def on_message(self, message_json):
         logging.info(message_json)
         message = json.loads(message_json)
         if message["messageType"] == "Hello":
-            isAligned = self.globals.state.alignment_matrices is not None
-            self.write_message(
-                IsAligned(isTelescopeAligned=isAligned).model_dump_json())
+            self._send_is_aligned()
             if self.send_task is None:
-                self.send_task = asyncio.create_task(self._send_coordinates())
+                self.send_task = asyncio.create_task(
+                    self._send_coordinates_periodically())
         else:
             raise ValueError(f"unexpected message {message}")
 
@@ -150,17 +153,33 @@ class WebsocketHandler(websocket.WebSocketHandler):
         if self.send_task:
             self.send_task.cancel()
             self.send_task = None
+        self.globals.state.unregister(
+            SystemState.ALIGN_CHANGE,
+            self._on_state_change)
 
     def check_origin(self, origin):
         return True
     
-    async def _send_coordinates(self):
+    def _on_state_change(self, event_name):
+        try:
+            self._send_is_aligned()
+            if self._is_aligned():
+                current_time = time.time()
+                current_taz_coords = self.telescope_interface.get_taz_coords()
+                self._send_telescope_coords(
+                    current_taz_coords,
+                    current_time)
+        except websocket.WebSocketClosedError:
+            logging.warn("Failed to send state change messages" +
+                        "because websocket is closed")
+
+    async def _send_coordinates_periodically(self):
         try:
             await self._do_send_coordinates()
         except Exception as e:
             logging.exception(e, exc_info=e)
             raise e
-
+    
     async def _do_send_coordinates(self):
         SLEEP_INTERVAL = .5
         PING_INTERVAL = 10
@@ -168,34 +187,16 @@ class WebsocketHandler(websocket.WebSocketHandler):
         previous_taz_coords = None
         previous_time = 0
         while True:
-            alignment_matrices = self.globals.state.alignment_matrices
-            is_aligned = alignment_matrices is not None
+            is_aligned = self._is_aligned()
             current_time = time.time()
             if is_aligned:
                 current_taz_coords = self.telescope_interface.get_taz_coords()
                 if (current_taz_coords != previous_taz_coords or
                     current_time - previous_time > PING_INTERVAL):
                     try:
-                        taz_coords = TazCoords(
-                            taz=current_taz_coords.taz,
-                            talt=current_taz_coords.talt)
-                        scope_az_coords = coordinates.taz_to_az(
-                            alignment_matrices,
-                            current_taz_coords.taz,
-                            current_taz_coords.talt)
-                        scope_az_coords = AltAzCoords(**scope_az_coords)
-                        eq_coords = coordinates.alt_az_to_eq(
-                            az=scope_az_coords.az,
-                            alt=scope_az_coords.alt,
-                            location=self.globals.state.location,
-                            timestamp=current_time)
-                        eq_coords = EqCoords(ra=eq_coords.ra.value,
-                                             dec=eq_coords.dec.value)
-                        scope_coords = TelescopeCoords(
-                            taz_coords=taz_coords,
-                            alt_az_coords=scope_az_coords,
-                            eq_coords=eq_coords)
-                        self.write_message(scope_coords.model_dump_json())
+                        self._send_telescope_coords(
+                            current_taz_coords,
+                            current_time)
                         previous_taz_coords = current_taz_coords
                         previous_time = current_time
                     except websocket.WebSocketClosedError:
@@ -204,8 +205,7 @@ class WebsocketHandler(websocket.WebSocketHandler):
             if (is_aligned and not previous_is_aligned or
                    (current_time - previous_time > PING_INTERVAL)):
                 try:
-                    self.write_message(
-                        IsAligned(isTelescopeAligned=is_aligned).model_dump_json())
+                    self._send_is_aligned()
                     previous_is_aligned = is_aligned
                     previous_time = current_time
                 except websocket.WebSocketClosedError:
@@ -217,3 +217,42 @@ class WebsocketHandler(websocket.WebSocketHandler):
                 logging.info("_send_coordinates canceled")
                 raise e
         print('I should not be here')
+    
+    def _is_aligned(self):
+        alignment_matrices = self.globals.state.alignment_matrices
+        return alignment_matrices is not None
+
+    def _send_is_aligned(self):
+        """Sends a IsAligned message asynchronously.
+
+            Throws websocket.WebSocketClosedError if the websocket is closed.
+        """
+        self.write_message(
+            IsAligned(isTelescopeAligned=self._is_aligned()).model_dump_json())
+
+    def _send_telescope_coords(self, current_taz_coords, current_time):
+        """Sends a TelescopeCoords message asynchronously.
+
+            Throws websocket.WebSocketClosedError if the websocket is closed.
+        """
+        taz_coords = TazCoords(
+            taz=current_taz_coords.taz,
+            talt=current_taz_coords.talt)
+        scope_az_coords = coordinates.taz_to_az(
+            self.globals.state.alignment_matrices,
+            current_taz_coords.taz,
+            current_taz_coords.talt)
+        scope_az_coords = AltAzCoords(**scope_az_coords)
+        eq_coords = coordinates.alt_az_to_eq(
+            az=scope_az_coords.az,
+            alt=scope_az_coords.alt,
+            location=self.globals.state.location,
+            timestamp=current_time)
+        eq_coords = EqCoords(ra=eq_coords.ra.value,
+                                dec=eq_coords.dec.value)
+        scope_coords = TelescopeCoords(
+            taz_coords=taz_coords,
+            alt_az_coords=scope_az_coords,
+            eq_coords=eq_coords)
+
+        self.write_message(scope_coords.model_dump_json())

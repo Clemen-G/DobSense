d75b24dce57c0b703e0ec4eab8af968753c87378
Clemen-G <3084704+Clemen-G@users.noreply.github.com>
2023-09-26 11:33:39 -0400

    added multiple zoom levels to PointingAid

--- frontend/src/app/component/PointingAid.js
+++ frontend/src/app/component/PointingAid.js
@@ -11,12 +11,22 @@ export default function PointingAid({scope_taz, scope_talt, target_taz, target_t
     const delta_taz = delta_taz_360 <= 180 ? delta_taz_360 : delta_taz_360 - 360
     const delta_talt = target_talt - scope_talt;
 
+    // az spans 360, alt 180. storing the ratio to base calculations on a single value.
+    const az_alt_ratio = 2;
     const canvas_x = 300;
-    const canvas_y = canvas_x / 2;
-    const is_zoom = Math.abs(delta_taz) < 10 && Math.abs(delta_talt) < 5;
-    const view_taz =  is_zoom ? 12 : 180;
-    const view_talt = is_zoom ? 6 : 90;
-    const search_circle_radius = is_zoom ? 1 : 5;
+    const canvas_y = canvas_x / az_alt_ratio;
+    // view will rescale if -e.g. dtaz < 30 && talt < 15
+    const rescale_thresholds = [
+        2, 5, 30, 180
+    ]
+    // zoom views get a margin so that the point shows well inside the drawing area on zoom in.
+    const margin_factor = 1.1;
+    const search_circle_radius = 1;
+
+    const active_threshold = rescale_thresholds.find(t => Math.max(Math.abs(delta_taz), Math.abs(delta_talt*2)) < t);
+    const view_taz = active_threshold == 180 ? active_threshold : active_threshold * margin_factor;
+    
+    const view_talt = view_taz / az_alt_ratio;
 
     useEffect(() => {
         if (containerRef.current) {
@@ -38,9 +48,14 @@ export default function PointingAid({scope_taz, scope_talt, target_taz, target_t
             draw.text("\u0394talt: "+delta_talt.toFixed(1))
                 .font({family:   'monospace', size: 14})
                 .move(0,32).stroke("red").fill("red");
-
-            draw.circle(search_circle_radius / view_taz * (canvas_x/2))
-                .fill("none").stroke("red")
+            
+            const search_circle_radius_px = Math.max(search_circle_radius / view_taz * (canvas_x/2), 5);
+            const search_circle_radius_fill = search_circle_radius / view_taz * (canvas_x/2) < 5 ?
+                "red" :
+                "none";
+            draw.circle(search_circle_radius_px)
+                .fill(search_circle_radius_fill)
+                .stroke("red")
                 .center(canvas_x/2, canvas_y/2);
             
             draw.circle(5).fill('#39f')
